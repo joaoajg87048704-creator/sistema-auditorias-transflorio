bdabebccdfb98566874ffdf9f7ee9dfdefe2ceb5
joaoajg87048704-creator <joaoajg87048704@gmail.com>
2026-08-18 13:05:51 -0300

    chore: remove temp file

--- fix-emergencia.js
+++
@@ -1,5 +0,0 @@
-// This is a migration script - apply the fix directly to index.html
-// Changes needed:
-// 1. HTML: Replace Base select with Motorista select in emergencias tab
-// 2. JS: Update getHeaderValues to use emer-input-motorista
-// 3. JS: Add emer-input-motorista to populateBaseSelects
